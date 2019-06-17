560fb946638497c6c0039846f4c48ddb8d8c43c8
Tangjiafeng <1282141182@qq.com>
2019-06-17 11:37:07 +0800

    ThreadLocal，以空间换时间的方式，将变量拷贝到本地，不用加锁，自己进行操作

---
+++ src/com/mashibing/c_022/ThreadLocalTest1.java
@@ -0,0 +1,39 @@
+package com.mashibing.c_022;
+
+import java.util.concurrent.TimeUnit;
+
+public class ThreadLocalTest1 {
+//	volatile Person p = new Person();
+	static ThreadLocal<Person> tlp = new ThreadLocal<Person>();
+	
+	public static void main(String[] args) {
+		ThreadLocalTest1 tl = new ThreadLocalTest1();
+		
+		new Thread(() -> {			
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+//			System.out.println(p.name);
+			System.out.println(tlp.get());
+		}).start();
+		
+		try {
+			TimeUnit.SECONDS.sleep(1);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		new Thread(() -> {
+			tlp.set(new Person());
+//			System.out.println(p.name);
+			System.out.println(tlp.get().name);
+		}).start();
+	}
+
+}
+
+class Person {
+	String name = "zhangsan";
+}
