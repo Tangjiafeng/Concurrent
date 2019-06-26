0f2d68a3987593a955759e90806cdbe652c8cc4b
Tangjiafeng <1282141182@qq.com>
2019-06-26 23:56:18 +0800

    线程池的基本概念

---
+++ src/com/mashibing/c_026/T_01_ThreadPool.java
@@ -0,0 +1,50 @@
+package com.mashibing.c_026;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class T_01_ThreadPool {
+
+	public static void main(String[] args) {
+		ExecutorService service = Executors.newFixedThreadPool(5);
+		
+		for (int i = 0; i < 6; i++) {
+			service.execute(() -> {
+				try {
+					Thread.sleep(500);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				
+				System.out.println(Thread.currentThread().getName());
+			});
+		}
+		
+		System.out.println(service);		
+		// 正常关闭，会等待任务执行结束
+		service.shutdown();
+		/*
+		 * 立即关闭，任务将被打断，企图立即停止，事实上不一定：跟 shutdown() 一样，先停止接收外部提交的任务，忽略队列里等待的任务，
+		 * 尝试将正在跑的任务 interrupt 中断，返回未执行的任务列表，它试图终止线程的方法是通过调用 Thread.interrupt() 方法
+		 * 来实现的，但是大家知道，这种方法的作用有限，如果线程中没有 sleep、wait、Condition、定时锁等应用, interrupt()
+		 * 方法是无法中断当前的线程的。所以，ShutdownNow() 并不代表线程池就一定立即就能退出，它也可能必须要等待所有正在执行的任务
+		 * 都执行完成了才能退出。但是大多数时候是能立即退出的。		
+		*/
+//		service.shutdownNow();
+		System.out.println(service);// shutting down
+		System.out.println(service.isShutdown());// true: shutting down		
+		System.out.println(service.isTerminated());// false
+		
+		try {
+			TimeUnit.SECONDS.sleep(5);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println(service);
+		System.out.println(service.isShutdown());
+		System.out.println(service.isTerminated());
+	}
+
+}
---
+++ src/com/mashibing/c_026/T_02_FutureTask.java
@@ -0,0 +1,48 @@
+package com.mashibing.c_026;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
+
+public class T_02_FutureTask {
+
+	public static void main(String[] args) {
+		FutureTask<Integer> task = new FutureTask<>(() -> {
+			Thread.sleep(1000);
+			return 1000;
+		});
+		
+		new Thread(task).start();
+		
+		try {
+			// get() 阻塞方法，只有任务结束，返回值返回才会执行。
+			System.out.println(task.get());
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			e.printStackTrace();
+		}
+		
+		ExecutorService service = Executors.newFixedThreadPool(5);
+		
+		Future<Integer> f = service.submit(() -> {
+			Thread.sleep(1000);
+			return 1;
+		});
+		
+		System.out.println(f.isDone());
+		try {
+			System.out.println(f.get());
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println(f.isDone());
+		service.shutdown();
+	}
+
+}
