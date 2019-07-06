cbaaa649964fe0b33fd62fbf9967ffb317953a61
Tangjiafeng <1282141182@qq.com>
2019-07-06 15:02:14 +0800

    抢夺任务的线程池

--- src/com/mashibing/c_026/T_07_WorkStealingPool.java
+++ src/com/mashibing/c_026/T_07_WorkStealingPool.java
@@ -1,10 +1,45 @@
 package com.mashibing.c_026;
 
+import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class T_07_WorkStealingPool {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// 打印 CPU 核数
 		System.out.println(Runtime.getRuntime().availableProcessors());
+		ExecutorService service = Executors.newWorkStealingPool(); 
+		
+		service.execute(new R(2000));
+		service.execute(new R(2000));
+		service.execute(new R(1000));
+		service.execute(new R(2000));
+		service.execute(new R(2000));
+		service.execute(new R(2000));
+		service.execute(new R(2000));
+		service.execute(new R(2000));
+		service.execute(new R(2000));// CPU核数此时为 8，故第九个任务会等待执行首先完毕线程，然后交给其执行。
+		
+		System.in.read();
 	}
 
+	static class R implements Runnable {
+		int time;
+
+		R(int t) {
+			this.time = t;
+		}
+
+		@Override
+		public void run() {
+			try {
+				Thread.sleep(this.time);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			System.out.println(Thread.currentThread().getName() + " " + this.time);
+		}
+		
+	}
 }
